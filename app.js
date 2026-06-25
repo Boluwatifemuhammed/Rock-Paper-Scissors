72c7d10d1853f86420872ed158c11d862751eaef
Boluwatife Muhammed <boluwatifemuhammed8@gmail.com>
2026-06-25 02:51:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const rockBtn = document.getElementById("rock-btn")
+const paperBtn = document.getElementById("paper-btn")
+const scissorsBtn = document.getElementById("scissors-btn")
+
+// console.log(rockBtn);
+
+rockBtn.addEventListener("click", () => {
+    const randomNumber = Math.random();
+
+    if (randomNumber >=0 && randomNumber < 1/3) {
+        console.log("rock")
+    } else if(randomNumber >= 1/3 && randomNumber < 2/3){
+        console.log("paper");
+        
+        
+    }
+    
+})
