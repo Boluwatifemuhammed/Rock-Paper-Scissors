5dbced4261799c8ca5a1651cd558e1207e82e917
Boluwatife Muhammed <boluwatifemuhammed8@gmail.com>
2026-06-25 02:56:38 +0100

    updated

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ rockBtn.addEventListener("click", () => {
 
     if (randomNumber >=0 && randomNumber < 1/3) {
         console.log("rock")
-    } else if(randomNumber >= 1/3 && randomNumber < 2/3){
+    } 
+    else if(randomNumber >= 1/3 && randomNumber < 2/3){
         console.log("paper");
-        
+    } else{
+        console.log("scissors");
         
     }
     
