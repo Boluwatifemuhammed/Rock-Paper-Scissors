25083548d3206b0c81629d15a1433c33785d77eb
Boluwatife Muhammed <boluwatifemuhammed8@gmail.com>
2026-06-25 03:11:35 +0100

    updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const buttons = document.querySelectorAll(".choice-btn")
 
 
 buttons.forEach((button) =>{
-    
+
     button.addEventListener("click", () => {
     const randomNumber = Math.random();
 
@@ -23,17 +23,3 @@ buttons.forEach((button) =>{
 
 });
 
-button.addEventListener("click", () => {
-    const randomNumber = Math.random();
-
-    if (randomNumber >=0 && randomNumber < 1/3) {
-        console.log("rock")
-    } 
-    else if(randomNumber >= 1/3 && randomNumber < 2/3){
-        console.log("paper");
-    } else{
-        console.log("scissors");
-        
-    }
-    
-})
